fc194a41bdca5fbcfcf35bba079e9092cec25156
Merve Bulut <47273479+merve-bulut@users.noreply.github.com>
2025-03-05 23:45:01 +0300

    Initial Commit

---
+++ UsernameApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UsernameApp
+//
+//  Created by Merve Bulut on 5.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsernameApp/UsernameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsernameAppApp.swift
+//  UsernameApp
+//
+//  Created by Merve Bulut on 5.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct UsernameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
