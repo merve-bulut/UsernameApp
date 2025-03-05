8956568387ea4e51b7a9f0d1ddcb6818cc07e5e1
Merve Bulut <47273479+merve-bulut@users.noreply.github.com>
2025-03-05 23:58:28 +0300

    Update ContentView.swift
    Initial commit

--- UsernameApp/ContentView.swift
+++ UsernameApp/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var message = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Please Enter Username", text: $username)
+                .padding()
+                .border(Color.gray, width: 1)
+                .cornerRadius(5)
+                .padding(.bottom, 20)
+            
+            Button(action: {
+                if username.isEmpty {
+                    message = "Please enter a username"
+                }
+                else {
+                    message = "Welcome, \(username)!"
+                }
+            }) {
+                Text("Send")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Text(message)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 20)
         }
         .padding()
     }
